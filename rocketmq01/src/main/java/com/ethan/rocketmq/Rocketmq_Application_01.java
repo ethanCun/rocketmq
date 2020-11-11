af56afcde1070bb4bc69b0ddeace6ddb5da6447f
macOfEthan <zz511627807@163.com>
2020-11-11 14:25:59 +0800

    1. 消息同步发送、异步发送、单向发送； 2. 消息消费的广播模式个负载均衡模式， 默认是负载均衡模式；

--- rocketmq01/src/main/java/com/ethan/rocketmq/Rocketmq_Application_01.java
+++ rocketmq01/src/main/java/com/ethan/rocketmq/Rocketmq_Application_01.java
@@ -1,10 +1,14 @@
 package com.ethan.rocketmq;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+@SpringBootApplication
 public class Rocketmq_Application_01 {
 
     public static void main(String[] args) {
 
-
+        SpringApplication.run(Rocketmq_Application_01.class, args);
     }
 }
